1709eacb680f7bd27bdcd17f78fc6ede546ad564
CuartoHokage <jaime199505@hotmail.com>
2018-10-07 11:08:03 -0500

    liberando espacios

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
 	port: process.env.PORT || 3000,
-	****base de datos local****//
+	//****base de datos local****//
 	//db: process.env.MONGODB || 'mongodb://localhost:27017/cine',
 	//****base de datos en la nube***//
 	db: process.env.MONGODB || 'mongodb://12qwaszx:12qwaszx@ds163510.mlab.com:63510/cine2',
