108606574db7c304631627b4fc5f3d7e40235489
CuartoHokage <jaime199505@hotmail.com>
2019-01-07 22:36:47 -0500

    arreglando vulnerabilidad event-stream eliminando flapmap-stream y cambiando version 3.3.4 de event

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports={
 	port: process.env.PORT || 3000,
 	//****base de datos local****//
-	//db: process.env.MONGODB || 'mongodb://localhost:27017/cine',
+	db: process.env.MONGODB || 'mongodb://localhost:27017/cine',
 	//****base de datos en la nube***//
-	db: process.env.MONGODB || 'mongodb://12qwaszx:12qwaszx@ds163510.mlab.com:63510/cine2',
+	// db: process.env.MONGODB || 'mongodb://12qwaszx:12qwaszx@ds163510.mlab.com:63510/cine2',
 	SECRET_TOKEN: 'miclavedetokens'
 }
